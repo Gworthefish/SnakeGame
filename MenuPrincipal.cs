01071373d5b5348ce403af1e4a4b3bb5e2a9226e
FabianHS <fabian.salazar4@ulatina.net>
2024-02-21 14:49:19 -0600

    Este es mi primer comit donde agrego un menu Principal, cambio la ventana principal del  F1 a Menu Principal y agrego la funcion de jugar al boton llamando la ventana de F1 y escondiendo la anterior ventanda

---
+++ MenuPrincipal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // Esconde el menú principal.
+            Form1 juego = new Form1();
+            juego.FormClosed += (s, args) => this.Close(); // Cerrar el menú principal cuando se cierre el juego.
+            juego.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
